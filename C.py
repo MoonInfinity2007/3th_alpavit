33484cfd5294ea709deab6c6a2f594e80b44b01a
moon2007 <mooninfinityalways@gmail/com>
2022-01-04 17:43:05 +0300

    C

---
+++ C.py
@@ -0,0 +1,11 @@
+def C():
+    print("     ########  ")
+    print("   ##        # ")
+    print("  #          # ")
+    print(" ##            ")
+    print(" #             ")
+    print(" ##            ")
+    print("   #         # ")
+    print("    ##     #   ")
+    print("      #####    ")
+C()
