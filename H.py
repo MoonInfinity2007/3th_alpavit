6583a7b70e1b2f914cd4ce91517ac871426ec5d5
moon2007 <mooninfinityalways@gmail/com>
2022-01-04 19:09:31 +0300

    H

---
+++ H.py
@@ -0,0 +1,11 @@
+def H():
+    print("  ##       ##  ")
+    print("  ##       ##  ")
+    print("  ##       ##  ")
+    print("  ##       ##  ")
+    print("  ###########  ")
+    print("  ##       ##  ")
+    print("  ##       ##  ")
+    print("  ##       ##  ")
+    print("  ##       ##  ")
+H()
